add6600814761ff12e4f387803c32c1007b16e1c
Kelly Deng <kelly.deng@stonybrook.edu>
2019-11-09 19:52:42 -0500

    Cleaned up

--- index.js
+++ index.js
@@ -1,37 +1,22 @@
 var express = require('express');
 var Memcached = require('memcached');
 var morgan = require('morgan');
+var mariadb = require('mariadb');
+
 
 var app = express();
 app.use(morgan('common'));
 
-var memcached = new Memcached('127.0.0.1:11211');
-
-const mariadb = require('mariadb');
 const pool = mariadb.createPool({host: 'localhost', user: 'root', database: 'hw8', connectionLimit: 5});
+const memcached = new Memcached('127.0.0.1:11211');
 
-async function asyncFunction(club, pos) {
-  // let results = memcached.get("club=" + club + "&pos=" + pos, function(err, res) {
-  //   if(err)
-  //     console.log("get error", err);
-  //   if(res) {
-  //     console.log("get result", res);
-  //     return JSON.parse(res);
-  //   }
-  // });
-  // console.log(results)
-  // if(results)
-  //   return JSON.parse(results);
-  
+async function queryDatabase(club, pos) {
   let conn;
   try {
-
     conn = await pool.getConnection();
     const assists = await conn.query("SELECT max(a) as max_assists, avg(a) as avg_assists FROM assists where club=? and pos=?", [club, pos]);
     const players = await conn.query("SELECT player FROM assists WHERE club=? AND pos=? and a=? ORDER BY a, gs DESC, player", [club, pos, assists[0].max_assists]);
-    // console.log(assists[0].max_assists, assists[0].avg_assists)
-    // console.log(players)
-    
+
     results = {
       "club": club,
       "pos": pos,
@@ -41,33 +26,30 @@ async function asyncFunction(club, pos) {
     }
 
     // either save the results as a json string, or just do res.send()
-    // otherwise it's getting jsonified twice
-    // console.log(JSON.stringify(results))
+    // otherwise it will get stringified twice
 
+    // saves results for 60 seconds
     memcached.set("club=" + club + "&pos=" + pos, results, 60, function (err, result) { 
       if(err)
         console.log("set error", err);
       if(result)
-        console.log("set result", result)
+        console.log("set result", result);
     });
     
     return results;
-
   } 
   catch(err) {
 	  throw err;
   } 
   finally {
     if(conn) 
-      conn.release(); //release to pool
+      conn.release();
   }
 }
 
 app.get('/hw8', function(req, res){
   let club = req.query.club;
   let pos = req.query.pos;
-
-  console.log(req.query)
 
   memcached.get("club=" + club + "&pos=" + pos, function(err, result) {
     if(err)
@@ -76,8 +58,8 @@ app.get('/hw8', function(req, res){
       console.log("get result", result);
       res.json(result);
     }
-    else {
-      asyncFunction(club, pos).then(response => {
+    else { // the else is important
+      queryDatabase(club, pos).then(response => {
         res.json(response);
       })
       .catch(err => {
@@ -87,5 +69,5 @@ app.get('/hw8', function(req, res){
   });
 });
 
-app.listen(5000);
-// app.listen(80);
+// app.listen(5000);
+app.listen(80);
